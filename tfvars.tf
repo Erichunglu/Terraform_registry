2dffcf3d81cdbdf69e7bca7901be16fdf6fadde2
Erichunglu <klteric1@gmail.com>
2021-05-26 17:29:18 +0800

    push

--- tfvars.tf
+++ tfvars.tf
@@ -1,7 +1,7 @@
-<!-- BEGIN_TF_DOCS -->
+#<!-- BEGIN_TF_DOCS -->
 instance-az    = "us-east-1a"
 instance-count = 1
 instance-type  = "t3.micro"
 region         = "us-east-1"
 server-name    = "cathay01"
-<!-- END_TF_DOCS -->
+#<!-- END_TF_DOCS -->
